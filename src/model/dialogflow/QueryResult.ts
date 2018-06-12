5848a6cad6b682c6aeb2fb76719c683fcf0a27a0
Thomas Marquis <thomas.marquis.dev@gmail.com>
2018-06-12 13:07:22 +0200

    add doc to WebhookRequest

--- src/model/dialogflow/QueryResult.ts
+++ src/model/dialogflow/QueryResult.ts
@@ -1,6 +1,10 @@
 import Context from "./Context";
 import Intent from "./Intent";
+import Message from "./Message";
 
+/**
+ * Represents the result of conversational query or event processing.
+ */
 export default class Queryresult {
   private queryText: String;
   private languageCode: String;
@@ -9,7 +13,7 @@ export default class Queryresult {
   private parameters: any;
   private allRequiredParamsPresent: Boolean;
   private fulfillmentText: String;
-  private fulfillmentMessages;
+  private fulfillmentMessages: Array<Message>;
   private webhookSource: String;
   private webhookPayload: any;
   private outputContexts: Array<Context>;
@@ -18,6 +22,280 @@ export default class Queryresult {
   private diagnosticInfo: any;
 
   constructor() {
+    this.fulfillmentMessages = new Array();
     this.outputContexts = new Array();
+  }
+
+  /**
+   * The original conversational query text:
+   *
+   * - If natural language text was provided as input, queryText contains a copy of the input.
+   * - If natural language speech audio was provided as input, queryText contains the speech
+   * recognition result. If speech recognizer produced multiple alternatives, a particular one is picked.
+   * - If an event was provided as input, queryText is not set.
+   *
+   * @return {String}
+   */
+  public getQueryText(): String {
+    return this.queryText;
+  }
+
+  /**
+   * The language that was triggered during intent detection.
+   *
+   * @return {String}
+   */
+  public getLanguageCode(): String {
+    return this.languageCode;
+  }
+
+  /**
+   * The Speech recognition confidence between 0.0 and 1.0. A higher number
+   * indicates an estimated greater likelihood that the recognized words are correct.
+   * The default of 0.0 is a sentinel value indicating that confidence was not set.
+   *
+   * This field is not guaranteed to be accurate or set. In particular this field isn't
+   * set for StreamingDetectIntent since the streaming endpoint has separate confidence
+   * estimates per portion of the audio in StreamingRecognitionResult.
+   *
+   * @return {Number}
+   */
+  public getSpeechRecognitionConfidence(): Number {
+    return this.speechRecognitionConfidence;
+  }
+
+  /**
+   * The action name from the matched intent.
+   *
+   * @return {String}
+   */
+  public getAction(): String {
+    return this.action;
+  }
+
+  /**
+   * The collection of extracted parameters.
+   *
+   * @return {any}
+   */
+  public getParameters(): any {
+    return this.parameters;
+  }
+
+  /**
+   * This field is set to:
+   *
+   * - false if the matched intent has required parameters and not all of
+   * the required parameter values have been collected.
+   * - true if all required parameter values have been collected, or if
+   * the matched intent doesn't contain any required parameters.
+   *
+   * @return {Boolean}
+   */
+  public getAllRequiredParamsPresent(): Boolean {
+    return this.allRequiredParamsPresent;
+  }
+
+  /**
+   * The text to be pronounced to the user or shown on the screen.
+   * @return {String}
+   */
+  public getFulfillmentText(): String {
+    return this.fulfillmentText;
+  }
+
+  /**
+   * If the query was fulfilled by a webhook call, this field is set to the
+   * value of the source field returned in the webhook response.
+   *
+   * @return {String}
+   */
+  public getWebhookSource(): String {
+    return this.webhookSource;
+  }
+
+  /**
+   * If the query was fulfilled by a webhook call, this field is set to the value
+   * of the payload field returned in the webhook response.
+   *
+   * @return {any}
+   */
+  public getWebhookPayload(): any {
+    return this.webhookPayload;
+  }
+
+  /**
+   * The collection of output contexts. If applicable, outputContexts.parameters contains
+   * entries with name <parameter name>.original containing the original parameter
+   * values before the query.
+   *
+   * @return {Array<Context>}
+   */
+  public getOutputContexts(): Array<Context> {
+    return this.outputContexts;
+  }
+
+  /**
+   * The intent that matched the conversational query. Some, not all fields are filled in this
+   * message, including but not limited to: name, displayName and webhookState.
+   *
+   * @return {Intent}
+   */
+  public getIntent(): Intent {
+    return this.intent;
+  }
+
+  /**
+   * The intent detection confidence. Values range from 0.0 (completely uncertain) to 1.0 (completely certain).
+   *
+   * @return {Number}
+   */
+  public getIntentDetectionConfidence(): Number {
+    return this.intentDetectionConfidence;
+  }
+
+  /**
+   * The free-form diagnostic info. For example, this field could contain webhook call latency.
+   *
+   * @return {any}
+   */
+  public getDiagnosticInfo(): any {
+    return this.diagnosticInfo;
+  }
+
+  /**
+   * The original conversational query text:
+   *
+   * - If natural language text was provided as input, queryText contains a copy of the input.
+   * - If natural language speech audio was provided as input, queryText contains the speech
+   * recognition result. If speech recognizer produced multiple alternatives, a particular one is picked.
+   * - If an event was provided as input, queryText is not set.
+   *
+   * @param {String} value
+   */
+  public setQueryText(value: String) {
+    this.queryText = value;
+  }
+
+  /**
+   * The language that was triggered during intent detection.
+   *
+   * @param {String} value
+   */
+  public setLanguageCode(value: String) {
+    this.languageCode = value;
+  }
+
+  /**
+   * The Speech recognition confidence between 0.0 and 1.0. A higher number
+   * indicates an estimated greater likelihood that the recognized words are correct.
+   * The default of 0.0 is a sentinel value indicating that confidence was not set.
+   *
+   * This field is not guaranteed to be accurate or set. In particular this field isn't
+   * set for StreamingDetectIntent since the streaming endpoint has separate confidence
+   * estimates per portion of the audio in StreamingRecognitionResult.
+   *
+   * @param {Number} value
+   */
+  public setSpeechRecognitionConfidence(value: Number) {
+    this.speechRecognitionConfidence = value;
+  }
+
+  /**
+   * The action name from the matched intent.
+   *
+   * @param {String} value
+   */
+  public setAction(value: String) {
+    this.action = value;
+  }
+
+  /**
+   * The collection of extracted parameters.
+   *
+   * @param {any} value
+   */
+  public setParameters(value: any) {
+    this.parameters = value;
+  }
+
+  /**
+   *This field is set to:
+   *
+   * - false if the matched intent has required parameters and not all of
+   * the required parameter values have been collected.
+   * - true if all required parameter values have been collected, or if
+   * the matched intent doesn't contain any required parameters.
+   *
+   * @param {Boolean} value
+   */
+  public setAllRequiredParamsPresent(value: Boolean) {
+    this.allRequiredParamsPresent = value;
+  }
+
+  /**
+   * The text to be pronounced to the user or shown on the screen.
+   *
+   * @param {String} value
+   */
+  public setFulfillmentText(value: String) {
+    this.fulfillmentText = value;
+  }
+
+  /**
+   * The collection of rich messages to present to the user.
+   *
+   * @return {Array<Message>}
+   */
+  public getFulfillmentMessages(): Array<Message> {
+    return this.fulfillmentMessages;
+  }
+
+  /**
+   * If the query was fulfilled by a webhook call, this field is set to the
+   * value of the source field returned in the webhook response.
+   *
+   * @param {String} value
+   */
+  public setWebhookSource(value: String) {
+    this.webhookSource = value;
+  }
+
+  /**
+   * If the query was fulfilled by a webhook call, this field is set to the value
+   * of the payload field returned in the webhook response.
+   *
+   * @param {any} value
+   */
+  public setWebhookPayload(value: any) {
+    this.webhookPayload = value;
+  }
+
+  /**
+   * The intent that matched the conversational query. Some, not all fields are filled in this
+   * message, including but not limited to: name, displayName and webhookState.
+   *
+   * @param {Intent} value
+   */
+  public setIntent(value: Intent) {
+    this.intent = value;
+  }
+
+  /**
+   * The intent detection confidence. Values range from 0.0 (completely uncertain) to 1.0 (completely certain).
+   *
+   * @param {Number} value
+   */
+  public setIntentDetectionConfidence(value: Number) {
+    this.intentDetectionConfidence = value;
+  }
+
+  /**
+   * The free-form diagnostic info. For example, this field could contain webhook call latency.
+   *
+   * @param {any} value
+   */
+  public setDiagnosticInfo(value: any) {
+    this.diagnosticInfo = value;
   }
 }
--- src/model/dialogflow/WebhookRequest.ts
+++ src/model/dialogflow/WebhookRequest.ts
@@ -1,9 +1,31 @@
 import OriginalDetectIntentRequest from "./OriginalDetectIntentRequest";
 import Queryresult from "./QueryResult";
 
+/**
+ * The request message for a webhook call.
+ */
 export default class WebhookRequest {
+  /**
+   * The unique identifier of detectIntent request session.
+   * Can be used to identify end-user inside webhook implementation.
+   * Format: projects/<Project ID>/agent/sessions/<Session ID>.
+   */
   private session: String;
+
+  /**
+   * The unique identifier of the response.
+   * Contains the same value as [Streaming]DetectIntentResponse.response_id.
+   */
   private responseId: String;
+
+  /**
+   * The result of the conversational query or event processing.
+   * Contains the same value as [Streaming]DetectIntentResponse.query_result.
+   */
   private queryResult: Queryresult;
+
+  /**
+   * Optional. The contents of the original request that was passed to [Streaming]sessions.detectIntent call.
+   */
   private originalDetectIntentRequest: OriginalDetectIntentRequest;
 }
